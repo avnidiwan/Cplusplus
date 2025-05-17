d139bd25a706c731b8e537d2ba13b9c203366805
avnidiwan <avnidiwan2845@gmail.com>
2025-05-17 10:16:11 +0530

    reverse of array

--- recursion/reverseArray.cpp
+++ recursion/reverseArray.cpp
@@ -1,25 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-int reverse(int l, int r, int n,int arr[]){
-if(l>=r) return 0;
-else{
-    swap(arr[l], arr[r]);
-    cout<<reverse(l+1,r-1, n, arr);
-}
-// cout<<"reversed array is: ";
-// for(int i=0; i<n; i++){
-// cout<<arr[i]<<endl;
-// }
+void reverse(int l, int r,int n,int arr[]){
+    if(l>=r) return ;
+    else{
+        swap(arr[l], arr[r]);
+        reverse(l+1, r-1, n, arr);
+    }
 }
 int main(){ 
 int n;
 cout<<"enter the size of an array: ";
 cin>>n;
-int arr[n];
 cout<<endl;
+int arr[n];
 cout<<"enter the values in array: ";
 for(int i=0; i<n; i++){
     cin>>arr[i];
 }
-reverse(0,n-1, n, arr);
+// int arr[]={1,2,3,4,5};
+reverse(0,n-1,n,arr);
+cout<<"reversed array is: ";
+for(int i=0; i<n; i++){
+    cout<<arr[i]<<endl;
+}
+
 }
