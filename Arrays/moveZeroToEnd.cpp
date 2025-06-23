716df07eda9e38339afe3631b90aba60810b243b
avnidiwan <avnidiwan2845@gmail.com>
2025-06-23 13:31:58 +0530

    Array

---
+++ Arrays/moveZeroToEnd.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int n;
+cin >> n;
+int a[n];
+for (int i = 0; i < n; i++) {
+    cin >> a[i];
+}
+
+
+//#1
+int index = 0;
+int countzero = 0;
+
+// Move non-zero elements forward
+for (int i = 0; i < n; i++) {
+    if (a[i] != 0) {
+        a[index++] = a[i];
+    } else {
+        countzero++;
+    }
+}
+
+// Fill trailing positions with zeros
+for (int i = index; i < n; i++) {
+    a[i] = 0;
+}
+
+// Output
+cout << countzero << endl;
+for (int i = 0; i < n; i++) {
+    cout << a[i] << " ";
+}
+  //tc-> O(n), SC-> O(1)  
+  
+  
+
+
+  //#2 using swapping
+  int j=-1;
+  for(int i=0; i<n; i++){
+    if(a[i]==0){
+      j=i;
+      break;
+    }
+  }
+  for(int i=j+1;i<n;i++){
+    if(a[i]!=0){
+        swap(a[i],a[j]);
+        j++;
+    }
+  }
+  for(int i=0; i<n; i++){
+    cout<<a[i]<<" ";
+  }
+}
