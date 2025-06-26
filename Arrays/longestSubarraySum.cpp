1b0e26945d43e2119b73a5a13a28c388bb551083
avnidiwan <avnidiwan2845@gmail.com>
2025-06-26 15:14:08 +0530

    Array

---
+++ Arrays/longestSubarraySum.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+    int idx=0;
+    int findsum;
+    int num=0;
+    cin>>findsum;
+    int sum=0;
+    for(int i=0; i<n; i++){
+        if(sum<findsum){
+        sum+=a[i];
+        }
+        else if(sum==findsum){
+            while(sum>0){
+                sum-=a[i-1];
+                num=num*10+a[i];
+                
+                i--;
+            }
+            cout<<num;
+        }
+        else{
+            sum-=a[idx];
+            idx++;
+        }
+    }
+}
