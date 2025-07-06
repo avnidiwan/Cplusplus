d553c5cd3a33efe814ac17793b042d11723bd0cc
avnidiwan <avnidiwan2845@gmail.com>
2025-07-06 11:21:08 +0530

    Array

---
+++ Arrays/nextPermutation.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int> a;
+    for(int i=0; i<n; i++){
+        int x; cin>>x;
+        a.push_back(x);
+    }
+    int index=-1;
+    for(int i=n-2; i>=0; i--){
+        if(a[i]<a[i+1]){
+            index=i;
+            break;
+        }
+    }
+    if(index==-1){
+         reverse(a.begin(), a.end());
+    }
+    for(int i=n-1; i>index; i--){
+        if(a[i]>a[index]){
+            swap(a[i], a[index]);
+            break;
+        }
+    }
+    reverse(a.begin()+index+1, a.end());
+
+    for(int i:a){
+        cout<<i<<" ";
+    }
+}
