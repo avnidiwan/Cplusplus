471cb1891bc1ef9100e9607d5bd3b915ca67cd8e
avnidiwan <avnidiwan2845@gmail.com>
2025-07-11 13:07:09 +0530

    Array

--- Arrays/LongestConsecutiveSeq.cpp
+++ Arrays/LongestConsecutiveSeq.cpp
@@ -6,22 +6,9 @@ int main(){
     for(int i=0; i<n; i++){
         int x; cin>>x; a.push_back(x);
     }
-    // sort(a.begin(), a.end());
-    // int j=0, count=1;
-    // for(int i=0; i<n-2; ){
-    //     if(a[i]+1==a[i+1]){
-    //     j=i;
-    //     count++;
-    //     i++;
-    //     }
-    //     else{
-    //         i++;
-    //     }
-    // }
-    // cout<<count;
-
-    int count=0;
     
+//only works well for only positive values
+    int count=0;
    int hash[100001]={0};
     for(int i=0; i<n; i++){     
         hash[a[i]]++;
@@ -30,4 +17,49 @@ int main(){
         count++;
     }}
     cout<<count;
+
+
+    //better solution
+    /*
+        if(nums.size()==0) return 0;
+    int n=nums.size(), count=0, longest=1, lastsmallest=INT_MIN;
+    sort(nums.begin(), nums.end());
+    for(int i=0; i<n; i++){
+        if(nums[i]-1==lastsmallest){
+            count++;
+            lastsmallest=nums[i];
+        }
+        else if(nums[i] !=lastsmallest){
+            count=1;
+            lastsmallest=nums[i];
+        }
+        longest=max(count,longest);
+    }
+    return longest;
+    */
+
+    
+
+    //optimal solution
+    /*
+    int n=nums.size(),  longest=1;
+     if(n==0) return 0;
+    unordered_set<int> s;
+    for(int i=0; i<n; i++){
+        s.insert(nums[i]);
+    }
+    for(auto i:s){
+        if(s.find(i - 1)== s.end()){
+            int count=1;
+            int x=i;
+            while(s.find(x+1) != s.end()){
+                x++;
+                count++;
+            }
+            longest=max(longest,count);
+        }
+    }
+    return longest;
+    }
+    */
 }
